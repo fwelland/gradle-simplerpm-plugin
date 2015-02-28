ba394a0cc3e38dc7281032fb3d1d11939dc21c88
Fred Welland <fred.welland@gmail.com>
2015-02-28 09:56:22 -0500

    apply base plugin as this plugin is applied

--- src/main/groovy/org/fhw/gradle/simplerpm/SimpleRPMPlugin.groovy
+++ src/main/groovy/org/fhw/gradle/simplerpm/SimpleRPMPlugin.groovy
@@ -2,12 +2,13 @@ package org.fhw.gradle.simplerpm
 
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.plugins.BasePlugin
 
 class SimpleRPMPlugin implements Plugin<Project> {
         
     @Override
     void apply(Project project) {        
-                
+        project.plugins.apply(BasePlugin.class)
         project.extensions.create("simplerpm", SimpleRPMPluginExtension)        
         project.task('rpm', type: MakeRPMTask)
     }       
