cab287d534e3cad9935b1a7d08ebfcc587eaccf6
Fred Welland <fred.welland@gmail.com>
2015-02-28 10:21:00 -0500

    remove base and appy maven

--- src/main/groovy/org/fhw/gradle/simplerpm/SimpleRPMPlugin.groovy
+++ src/main/groovy/org/fhw/gradle/simplerpm/SimpleRPMPlugin.groovy
@@ -3,12 +3,14 @@ package org.fhw.gradle.simplerpm
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 import org.gradle.api.plugins.BasePlugin
+import org.gradle.api.plugins.MavenPlugin
 
 class SimpleRPMPlugin implements Plugin<Project> {
         
     @Override
     void apply(Project project) {        
-        project.plugins.apply(BasePlugin.class)
+        //project.plugins.apply(BasePlugin)
+        project.plugins.apply(MavenPlugin)
         project.extensions.create("simplerpm", SimpleRPMPluginExtension)        
         project.task('rpm', type: MakeRPMTask)
     }       
